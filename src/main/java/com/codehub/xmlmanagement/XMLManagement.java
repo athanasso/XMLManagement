53de6df1d559bae79b0ab3279bed51984eb9d63d
Emmanouil Athanasopoulos <manos.athanasopoulos@yahoo.gr>
2022-12-18 19:18:39 +0200

    Main

--- src/main/java/com/codehub/xmlmanagement/XMLManagement.java
+++ src/main/java/com/codehub/xmlmanagement/XMLManagement.java
@@ -1,10 +1,13 @@
 package com.codehub.xmlmanagement;
 
 import com.codehub.xmlmanagement.service.RawTextToXml;
+import com.codehub.xmlmanagement.service.XMLStatistics;
+import com.codehub.xmlmanagement.usecase.UseCase;
 
 public class XMLManagement {
 
     public static void main(String[] args) {
-        RawTextToXml.Run();
+        //RawTextToXml.Run();
+        UseCase.Start();
     }
 }
