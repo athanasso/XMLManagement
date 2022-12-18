e0841738f3ac40147d46aa46588f179b2ae9e757
Emmanouil Athanasopoulos <manos.athanasopoulos@yahoo.gr>
2022-12-18 19:18:22 +0200

    Statistics Model

---
+++ src/main/java/com/codehub/xmlmanagement/Model/Statistics.java
@@ -0,0 +1,88 @@
+package com.codehub.xmlmanagement.Model;
+
+import java.util.Date;
+
+public class Statistics {
+    private int paragraphs;
+    private int sentences;
+    private int words;
+    private int distinctWords;
+    private Date creationDate;
+    private String author;
+    private String appClassName;
+
+    public Statistics(int paragraphs, int sentences, int words, int distinctWords, Date creationDate, String author, String appClassName) {
+        this.paragraphs = paragraphs;
+        this.sentences = sentences;
+        this.words = words;
+        this.distinctWords = distinctWords;
+        this.creationDate = creationDate;
+        this.author = author;
+        this.appClassName = appClassName;
+    }
+
+    public Statistics() {
+    }
+    
+    public int getParagraphs() {
+        return paragraphs;
+    }
+
+    public void setParagraphs(int paragraphs) {
+        this.paragraphs = paragraphs;
+    }
+
+    public int getSentences() {
+        return sentences;
+    }
+
+    public void setSentences(int sentences) {
+        this.sentences = sentences;
+    }
+
+    public int getWords() {
+        return words;
+    }
+
+    public void setWords(int words) {
+        this.words = words;
+    }
+
+    public int getDistinctWords() {
+        return distinctWords;
+    }
+
+    public void setDistinctWords(int distinctWords) {
+        this.distinctWords = distinctWords;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getAppClassName() {
+        return appClassName;
+    }
+
+    public void setAppClassName(String appClassName) {
+        this.appClassName = appClassName;
+    }
+
+    @Override
+    public String toString() {
+        return "Statistics{" + "paragraphs=" + paragraphs + ", sentences=" + sentences + ", words=" + words + ", distinctWords=" + distinctWords + ", creationDate=" + creationDate + ", author=" + author + ", appClassName=" + appClassName + '}';
+    }
+
+}
